7941efb8ad8194cfce7b20660da5144a9345e0bd
Matt Corallo <git@bluematt.me>
2016-12-14 14:50:34 -0800

    Call UDPRelayBlock from ProcessNewBlock when appropriate

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2586,6 +2586,8 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             ProcessNewBlock(chainparams, pblock, /*fForceProcessing=*/true, &fNewBlock);
             if (fNewBlock) {
                 pfrom->nLastBlockTime = GetTime();
+                if (fLogIPs)
+                    LogPrint(BCLog::BENCH, "Block %s provided by %s\n", resp.blockhash.ToString(), pfrom->addr.ToString());
             } else {
                 LOCK(cs_main);
                 mapBlockSource.erase(pblock->GetHash());
@@ -2641,6 +2643,8 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         ProcessNewBlock(chainparams, pblock, forceProcessing, &fNewBlock);
         if (fNewBlock) {
             pfrom->nLastBlockTime = GetTime();
+            if (fLogIPs)
+                LogPrint(BCLog::BENCH, "Block %s provided by %s\n", pblock->GetHash().ToString(), pfrom->addr.ToString());
         } else {
             LOCK(cs_main);
             mapBlockSource.erase(pblock->GetHash());
