6b931e282ff90ef5aed597f5e1042501b16c6825
Abylay Ospan <aospan@netup.ru>
2018-05-20 09:06:44 -0500

    translate TERM signal to bitcoind process
    When stoppping docker container signal is received by PID=1 (bash) and
    do not translated to bitcoind process.
    bitcoind stoppping process can take about 20 seconds, so specify '-t 60'
    to 'docker stop' command.
    
    Signed-off-by: Abylay Ospan <aospan@netup.ru>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+trap 'kill -TERM $PID; wait $PID' TERM INT
+
 #wait until /tmp/async_rx appear
 while [ ! -e /tmp/async_rx ]
 do
@@ -10,4 +12,9 @@ done
 
 #start bitcoind
 /opt/bitcoinfibre/src/bitcoind -fecreaddevice=/tmp/async_rx -fecstat=60 \
-  -conf=/opt/bitcoin/bitcoin.conf -datadir=/opt/bitcoin/
+  -conf=/opt/bitcoin/bitcoin.conf -datadir=/opt/bitcoin/ &
+PID=$!
+wait $PID
+trap - TERM INT
+wait $PID
+EXIT_STATUS=$?
